5c4ec59c68868970b51b5df6012322d69b2bdeeb
Wasth <riasta@yandex.ru>
2020-01-10 11:41:17 +0300

    Testing Apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App(props) {
-	return <h1>Hello World</h1>
-}
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
+import { ExchangeRates } from './testfetch';
+
+const client = new ApolloClient({
+		cache: new InMemoryCache(),
+		link: new HttpLink({
+		uri: 'https://48p1r2roz4.sse.codesandbox.io',
+	})
+});
+
+function App(props){
+	return <ApolloProvider client={client}>
+		<h1>Hello World 🚀</h1>
+		<ExchangeRates />
+	</ApolloProvider>
+}
 ReactDOM.render(<App />, document.querySelector('#app'))
